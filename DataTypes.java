dda8a8387ec2b8497f5f75dbdbe3e26c7d526d60
Nishu Priya <128479787+NishuPriya05@users.noreply.github.com>
2024-05-02 21:01:02 +0530

    DataTypes

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        // 1.Primitive DataTypes
+
+        int myNum = 9;
+        float myFloat = 8.99f;
+        char myLetter = 'A';
+       boolean myBool = true;
+
+       System.out.println( myNum);
+       System.out.println( myFloat);
+       System.out.println( myLetter);
+       System.out.println( myBool);
+
+       // 2.Typecasting
+
+       double myDouble = 9.78d;
+       int myInt = (int)myDouble;  // syntax of typecasting :(pass the value that ou wanted to be converted ) old value
+       System.out.println(myInt);
+    }
+}
