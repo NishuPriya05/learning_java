1bcca2bbed93a8806c27c886e2ce4b7ae68c23c6
Nishu Priya <128479787+NishuPriya05@users.noreply.github.com>
2024-05-02 21:02:45 +0530

    About Variables

---
+++ variables.java
@@ -0,0 +1,25 @@
+public class variables{
+    public static void main(String[]args){
+
+      // 1.Create a variable named carName and assign the value Volvo to it.
+
+      String carName = "Volvo";
+      System.out.println(carName);
+
+      // 2.Create a variable named maxSpeed and assign the value 120 to it.
+
+      int maxSpeed = 120;
+      System.out.println(maxSpeed);
+
+      // 3.Display the sum of 5 + 10, using two variables: x and y.
+
+      int x = 5,y = 10;
+      System.out.println(x+y);
+
+      // 4.Create a variable called z, assign x + y to it, and display the result.
+      int z = x + y;
+      System.out.println(z);
+
+
+    }
+}
